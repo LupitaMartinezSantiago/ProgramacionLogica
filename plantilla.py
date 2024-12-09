3306b1941a3ff6440780006cde41f2a4e886a2f5
LupitaMartinezSantiago <167836035+LupitaMartinezSantiago@users.noreply.github.com>
2024-12-08 22:55:58 -0600

    Add files via upload
    es el codigo de una librewria

---
+++ plantilla.py
@@ -0,0 +1,22 @@
+# plantilla.py
+import tkinter as tk
+
+def centrar_frame_principal(window):
+    # Función para centrar la ventana en la pantalla
+    window.update_idletasks()
+    width = window.winfo_width()
+    height = window.winfo_height()
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+    position_top = int(screen_height / 2 - height / 2)
+    position_right = int(screen_width / 2 - width / 2)
+    window.geometry(f'{width}x{height}+{position_right}+{position_top}')
+
+def crear_label(text, window):
+    return tk.Label(window, text=text, font=("Arial", 12))
+
+def create_entry(window):
+    return tk.Entry(window, font=("Arial", 12))
+
+def create_button(window, text, command):
+    return tk.Button(window, text=text, command=command, font=("Arial", 12))
